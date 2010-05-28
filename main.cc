dbcb32921ac0a9fe87bd88c3851242445e2a27c7
Vimalkumar <j.vimal@gmail.com>
2010-05-27 22:29:05 -0700

    build_from from hovav's paper

--- main.cc
+++ main.cc
@@ -6,6 +6,7 @@ using namespace std;
 
 /* 2M buffer */
 unsigned char code[1<<21];
+Trie<Instruction> trie;
 
 int read_code(char *f) {
   int len = 0, num;
@@ -25,13 +26,52 @@ int read_code(char *f) {
   return len;
 }
 
+void build_from(
+  X86Disasm &d, 
+  int pos, 
+  Instruction ins, 
+  vector<Instruction> &v, 
+  int depth = 0) 
+{
+  if(depth == 20) {
+    return;
+  }
+  v.resize(depth+1);
+  v[depth] = ins;
+  
+  // max len of x86 instruction = 22, but let's stick to 6
+  FOR(step,1,22+1) {
+    Instruction temp = d.get_instruction(code+pos-step, step);
+    if(temp && temp.ins.size == step && temp.ins.bytes[0] != 0xc9 && temp.ins.bytes[0] != 0xc3) {
+      build_from(d, pos-step, temp, v, depth+1);
+    }
+  }
+  
+  if(depth > 1) {
+    reverse(v.begin(), v.end());
+    if(trie.insert(v)) {
+      printf("inserting sequence of length: %d\n", v.size());
+      EACH(ins, v) {
+        ins->print();
+      }
+    }
+    reverse(v.begin(), v.end());
+    printf("   \n");
+  }
+}
+
 int main() {
   int len = read_code("binsh.txt");
   X86Disasm d(code, len);
   
   Instruction ins;
   while(ins = d.next_instruction()) {
-    ins.print();
+    if(ins.ins.bytes[0] == 0xc3) {
+      /* This is a ret instruction */
+      printf("********** ret encountered at offset %d\n", ins.ins.offset);
+      vector<Instruction> v;
+      build_from(d, ins.ins.offset, ins, v);
+    }
   }
   
 	return 0;
